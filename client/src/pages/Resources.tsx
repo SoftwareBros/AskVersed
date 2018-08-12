e545cf6e6c3e1d43ec8da576b33231d859748cd3
Henry <henrythefoster@gmail.com>
2018-08-12 15:57:30 -0400

    resources basic

--- client/src/pages/Resources.tsx
+++ client/src/pages/Resources.tsx
@@ -1,25 +1,41 @@
+
+
 import * as React from "react";
 import * as compJSON from './Resources-components/av_comp.js';
 import * as progJSON from './Resources-components/av_prog.js';
 
-export default class Resources extends React.Component<{}, any>{
+export default class Resources extends React.Component<any, any>{
   public constructor(props: any) {
     super(props);
-
+    compJSON.push.apply(compJSON,progJSON);
     this.state = {
-      comp: compJSON,
-      prog: progJSON
-      
+      all: compJSON
     }
   }
-  
+
+
   public render(){
-  	// tslint:disable-next-line:no-console
-  	console.log(this.state.prog)
-  	// tslint:disable-next-line:no-console
-  	console.log(this.state.comp)
-  	return(
-  		<p>Hello</p>
-  	)
+    // tslint:disable-next-line:no-console
+    console.log(this.state.ahings)
+    // tslint:disable-next-line:no-console
+    console.log(this.state.comp)
+    return(
+      this.state.all.map( (obj: any)=> {
+        return(
+
+        <div className="card" key={/* tslint:disable:no-string-literal */"card"+obj["Name"]}>
+        <div className="card-body">
+          <h4 className="card-title">{/* tslint:disable:no-string-literal */obj["Categoraization within STEM"]}</h4>
+          <h5 className="card-title">{/* tslint:disable:no-string-literal */obj["Name"]}</h5>
+          <p className="card-text">
+            {/* tslint:disable:no-string-literal */obj["Description"]}
+          </p>
+          <a href={/* tslint:disable:no-string-literal */obj["URL"]} className="btn btn-primary">Website</a>
+        </div>
+      </div>
+      )
+      })
+      
+    );
   }
 }
